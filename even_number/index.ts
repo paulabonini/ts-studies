a5a04054ff24d1273da2bee9d72114a0747ff9f8
Paula <paula.bonini@bexsbanco.com.br>
2021-04-09 13:28:23 -0300

    even_number exercise

---
+++ even_number/index.ts
@@ -0,0 +1,14 @@
+const number = 1347;
+
+function verifify (number: number) {
+    if (number % 2 === 0) {
+        console.log(`O número ${number} é par`)
+    } else {
+        console.log(`O número ${number} é ímpar`)
+    }
+}
+
+verifify(number);
+
+
+// console.log(13%2)
