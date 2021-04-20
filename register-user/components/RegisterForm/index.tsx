09d9e224c49b353dbae6d6f46f7564744f47e0ae
paulabonini <paula.bonini@bexsbanco.com.br>
2021-04-20 12:33:34 -0300

    register-user creating  context

--- register-user/components/RegisterForm/index.tsx
+++ register-user/components/RegisterForm/index.tsx
@@ -1,20 +1,45 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+import { UserContext } from "../../contexts/UserContext/UserContext";
 import { Button, Form, FormField, Input } from "./style";
 
 export default function RegisterForm() {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+    console.log(name);
+  };
+
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    router.push({
+      pathname: "/user-page",
+    });
+  }
+
   return (
-    <Form action="">
+    <Form onSubmit={handleSubmit} name="form" id="form">
       <FormField>
-        <label htmlFor="completedName">Nome completo</label>
-        <Input type="text" id="completedName" minLength={3} required />
+        <label htmlFor="name">Nome completo</label>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          minLength={3}
+          required
+          onChange={handleChange}
+        />
       </FormField>
       <FormField>
         <label htmlFor="birthDay">Data de nascimento</label>
-        <Input type="date" id="birthDay" required />
+        <Input type="date" id="birthDay" name="birthDay" required />
       </FormField>
       <FormField>
         <label htmlFor="cpf">CPF</label>
-        <Input type="text" maxLength={11} id="cpf" required />
+        <Input type="text" maxLength={11} id="cpf" name="cpf" required />
       </FormField>
       <FormField>
         <label htmlFor="profession">Profissão</label>
@@ -26,3 +51,7 @@ export default function RegisterForm() {
 }
 
 // 2. Não se preocupe com o formato do CPF, somente números no campo
+
+//após o preechimento do formulário, redirecionar para a página de perfil de usuário para que possa verificar minhas informações cadastradas no sistema
+//-- Use Query parameters
+//utilizar o recurso de roteamento do NextJS para passar informação de uma página pra outra
---
+++ register-user/pages/user-page.tsx
@@ -0,0 +1,7 @@
+export default function UserPage() {
+  return (
+    <main>
+      <h1>name</h1>
+    </main>
+  );
+}
